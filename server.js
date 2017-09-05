44fad4dfbf1531604518a1db4f0b0af016a8babc
comp500 <comp500@users.noreply.github.com>
2017-09-05 17:36:28 +0100

    Add automatic broadcast address detection

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 const port = 43653;
-const broadcastAddr = "192.168.1.255";
 const dgram = require('dgram');
 const server = dgram.createSocket("udp4");
+const tocidr = require('subnet2cidr').cidrize;
+const netmask = require('netmask').Netmask;
+var broadcastAddresses;
 
 server.bind(function () {
 	server.setBroadcast(true);
+	broadcastAddresses = getBroadcastAddresses();
 	setInterval(broadcastNew, 3000);
 });
 
 function broadcastNew() {
 	var message = new Buffer("Broadcast message!");
-	server.send(message, 0, message.length, port, broadcastAddr, function () {
-		console.log("Sent '" + message + "'");
+	for (var i = 0; i < broadcastAddresses.length; i++) {
+		server.send(message, 0, message.length, port, broadcastAddresses[i], function () {
+			console.log("Sent '" + message + "'");
+		});
+	};
+}
+
+function getBroadcastAddresses() {
+	var interfaces = require('os').networkInterfaces();
+	var addrs = [];
+
+	Object.keys(interfaces).forEach(function (key) {
+		var interface = interfaces[key];
+		for (var i = 0; i < interface.length; i++) {
+			if (!interface[i].internal && interface[i].family == "IPv4" && interface[i].netmask && interface[i].address) {
+				var cidr_bits = 0;
+				interface[i].netmask.split('.').forEach(function (octet) {
+					cidr_bits += ((octet >>> 0).toString(2).match(/1/g) || []).length;
+				});
+				var addr = new netmask(interface[i].address + "/" + cidr_bits);
+				addrs.push(addr.broadcast);
+			}
+		}
 	});
+	return addrs;
 }
