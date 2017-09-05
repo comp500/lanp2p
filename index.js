287a6cf6a54fd38254b655a995bf349ac93f862c
comp500 <comp500@users.noreply.github.com>
2017-09-05 16:52:08 +0100

    Add test server

---
+++ index.js
@@ -0,0 +1,16 @@
+const port = 43653;
+const broadcastAddr = "255.255.255.255";
+const dgram = require('dgram');
+const server = dgram.createSocket("udp4");
+
+server.bind(function () {
+	server.setBroadcast(true);
+	setInterval(broadcastNew, 3000);
+});
+
+function broadcastNew() {
+	var message = new Buffer("Broadcast message!");
+	server.send(message, 0, message.length, port, broadcastAddr, function () {
+		console.log("Sent '" + message + "'");
+	});
+}
