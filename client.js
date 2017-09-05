d24dba9fc8cab1f89ee41a57e95f1a3ce3d43ecb
comp500 <comp500@users.noreply.github.com>
2017-09-05 16:53:13 +0100

    Add client

---
+++ client.js
@@ -0,0 +1,15 @@
+const port = 43653;
+const dgram = require('dgram');
+const client = dgram.createSocket('udp4');
+
+client.on('listening', function () {
+	var address = client.address();
+	console.log('UDP Client listening on ' + address.address + ":" + address.port);
+	client.setBroadcast(true);
+});
+
+client.on('message', function (message, rinfo) {
+	console.log('Message from: ' + rinfo.address + ':' + rinfo.port + ' - ' + message);
+});
+
+client.bind(port);
